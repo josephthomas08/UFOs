c077110df5e5d669310178782e8cc4855b1a39ad
josephthomas08 <joseph@tmgcontrol.com>
2021-03-13 12:14:32 -0500

    UFOs sightings with JS html bootstrap and css

--- js/app.js
+++ js/app.js
@@ -33,10 +33,10 @@ function updateFilters() {
   let changedElement = d3.select(this);
     // 4b. Save the value that was changed as a variable.
   let elementValue = changedElement.property('value');
-  // console.log(elementValue);
+  console.log(elementValue);
     // 4c. Save the id of the filter that was changed as a variable.
   let filteredId = changedElement.attr('id');
-  // console.log(filteredId);
+  console.log(filteredId);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
